089798969b1db3a1009e8fc0a2cae838595e997a
Rex146 Rexov <rex146@yandex.ru>
2021-10-17 00:34:05 +0300

    init new tasks

---
+++ task_22/solution.jl
@@ -0,0 +1,13 @@
+using HorizonSideRobots
+include("../basics.jl")
+
+
+md"""
+        # Подсчитать число прямоугольных перегородок (отрезков нет)
+"""
+
+
+ """Решение задачи 22"""
+ function solve_22!(r)
+        
+end
---
+++ task_24/solution.jl
@@ -0,0 +1,13 @@
+using HorizonSideRobots
+include("../basics.jl")
+
+
+md"""
+        # Найти площадь наибольшей прямоугольной перегородки
+"""
+
+
+ """Решение задачи 24"""
+ function solve_24!(r)
+        
+end

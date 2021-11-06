a74dd2aabca11d27a2fc74efaab35a8d7e50344d
x0y0eg <rex146@yandex.ru>
2021-11-06 19:27:57 +0300

    :sparkles: some TAB changes and add solution for 23rd task

--- task_22/solution.jl
+++ task_22/solution.jl
@@ -1,6 +1,4 @@
-using HorizonSideRobots
-include("../basics.jl")
-
+include("../task_20/solution.jl")
 
 md"""
         # Подсчитать число прямоугольных перегородок (отрезков нет)
@@ -9,5 +7,5 @@ md"""
 
 """Решение задачи 22"""
 function solve_22!(r)
-    
+    return solve_20!(r)
 end
